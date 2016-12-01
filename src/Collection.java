5a673d60c7a394ba1f52eab872f3d661510797e2
Wei Huang <weih81888@gmail.com>
2016-12-01 00:03:10 -0500

    File code in main

--- src/Collection.java
+++ src/Collection.java
@@ -21,5 +21,39 @@ public class Collection{
          String str = li;
          System.out.print(str + " ");
       }
+      
+      
+      // compare two file path
+      File file1 = new File("C:/File/demo1.txt");
+      File file2 = new File("C:/java/demo1.txt");
+      if(file1.compareTo(file2) == 0) {
+         System.out.println("Both paths are same!");
+      } else {
+         System.out.println("Paths are not same!");
+      }
+      
+      // create new file
+      try{
+          File file = new File("C:/myfile.txt");
+          if(file.createNewFile())
+          System.out.println("Success!");
+          else
+          System.out.println("Error, file already exists.");
+      }
+      catch(IOException ioe) {
+         ioe.printStackTrace();
+      }
+      
+      // Last Modification date of a file 
+      File file11 = new File("Main.java");
+      Long lastModified = file11.lastModified();
+      Date date = new Date(lastModified);
+      System.out.println(date);
+      
+      // Check the file if exits
+      File file21 = new File("C:/java.txt");
+      System.out.println(file21.exists());
+      
+      
    }
 }
