7c7432869c14862cd638b4b944493202166f960b
Wei Huang <weih81888@gmail.com>
2016-11-30 23:39:12 -0500

    Addddddddddddddddddddddddddddddd

--- src/Collection.java
+++ src/Collection.java
@@ -1,4 +1,25 @@
+import java.util.*;
+import java.io.*;
 
-public class Collection {
-
+public class Collection{
+	
+   public static void main(String args[]) throws IOException{
+      
+	   BufferedReader in = new BufferedReader (new InputStreamReader(System.in));
+      System.out.println("How many elements you want to add to the array: ");
+      
+      int n = Integer.parseInt(in.readLine());
+      String[] name = new String[n];
+      
+      for(int i = 0; i < n; i++){
+         name[i] = in.readLine();
+      }
+      
+      List<String> list = Arrays.asList(name); 
+      System.out.println();
+      for(String li: list){
+         String str = li;
+         System.out.print(str + " ");
+      }
+   }
 }
